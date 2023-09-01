51e4c8c9ca48d832b19552c5e6b8af5ba8864849
jpnorena1 <73927801+jpnorena1@users.noreply.github.com>
2023-09-01 17:14:21 -0500

    comments

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-def find_pairs_with_sum(numbers, target_sum):
-    num_set = set()  # To keep track of seen numbers
-    result = []
-    for num in numbers:
-        complement = target_sum - num
-        if complement in num_set:
-            result.append((num, complement))
-        num_set.add(num)
-    return result
-def main():
-    import sys
-    if len(sys.argv) < 3:
-        print("Usage: python app.py <list_of_numbers> <target_sum>")
-        return
-    numbers = list(map(int, sys.argv[1].split(',')))
-    target_sum = int(sys.argv[2])
-    pairs = find_pairs_with_sum(numbers, target_sum)
-    for pair in pairs:
-        print("+", pair[0], ",", pair[1])
-if __name__ == "__main__":
-    main()
+import unittest
+
+# Define a test class that inherits from unittest.TestCase.
+class TestFindPairsWithSum(unittest.TestCase):
+    def test_find_pairs_with_sum(self):
+        # Test case 1: Basic test case
+        numbers = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+        target_sum = 12
+        expected_result = [(5, 7), (16, -4), (12, 0)]
+        
+        # Check if the function find_pairs_with_sum produces the expected result.
+        self.assertEqual(find_pairs_with_sum(numbers, target_sum), expected_result)
+
+        # Test case 2: No pairs that sum up to the target
+        numbers = [1, 2, 3, 4, 5]
+        target_sum = 20
+        expected_result = []
+        
+        # Check if the function handles the case when there are no pairs that sum up to the target.
+        self.assertEqual(find_pairs_with_sum(numbers, target_sum), expected_result)
+
+        # Test case 3: Negative numbers
+        numbers = [-1, -2, -3, -4, -5]
+        target_sum = -5
+        expected_result = [(-1, -4), (-2, -3)]
+        
+        # Check if the function handles negative numbers and produces the correct result.
+        self.assertEqual(find_pairs_with_sum(numbers, target_sum), expected_result)
+
+if __name__ == '__main__':
+    # Run the unit tests.
+    unittest.main()
